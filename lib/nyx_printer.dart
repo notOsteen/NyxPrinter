2cf0d31207d822efd1f4f316de1bd5d3420544e7
Paul Poyyayil <paulpoyyayil.com>
2024-11-08 16:50:02 +0530

    init

---
+++ lib/nyx_printer.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/services.dart';
+import 'nyx_printer_platform_interface.dart';
+import 'nyx_text_format.dart';
+import 'dart:typed_data'; // Make sure to import this for Uint8List
+
+export 'nyx_text_format.dart';
+
+class NyxPrinter {
+  /// [getVersion] will return printer version number.
+  /// success result is 0.
+  Future<int?> getVersion() {
+    return NyxPrinterPlatform.instance.getVersion();
+  }
+
+  /// [printText] will print text
+  /// to format text you can use [NyxTextFormat] to set font size or font style ...etc
+  /// success result is 0.
+  Future<int?> printText(String text, {NyxTextFormat? textFormat}) {
+    return NyxPrinterPlatform.instance
+        .printText(text, textFormat ?? NyxTextFormat());
+  }
+
+  /// [printBarcode] will print barcode and you can set width and height.
+  /// default width is 300
+  /// default height is 160
+  /// success result is 0.
+  Future<int?> printBarcode(String text, {int? width, int? height}) {
+    return NyxPrinterPlatform.instance
+        .printBarcode(text, width ?? 300, height ?? 160);
+  }
+
+  /// [printQrCode] will print QR code and you can set width and height.
+  /// default width is 300
+  /// default height is 300
+  /// success result is 0.
+  Future<int?> printQrCode(String text, {int? width, int? height}) {
+    return NyxPrinterPlatform.instance
+        .printQrCode(text, width ?? 300, height ?? 300);
+  }
+
+  /// [printImage] will print only png images.
+  /// success result is 0.
+  Future<int?> printImage(Uint8List bytes) {
+    return NyxPrinterPlatform.instance.printBitmap(bytes);
+  }
+
+  /// [paperOut] will feed paper by the specified size.
+  /// success result is 0.
+  Future<int?> paperOut(int size) {
+    return NyxPrinterPlatform.instance.paperOut(size);
+  }
+
+  /// [paperOutText] will feed paper based on the text size.
+  /// success result is 0.
+  Future<int?> paperOutText(int size) {
+    return NyxPrinterPlatform.instance.paperOutText(size);
+  }
+}
